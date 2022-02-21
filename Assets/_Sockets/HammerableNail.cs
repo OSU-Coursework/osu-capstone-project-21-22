62f40d85260aa389b858d2f466cd0f2343340c42
Casey Nord <casey.nord@protonmail.com>
2022-02-20 21:51:23 -0800

    HammerableNail now extends from Socketable

--- Assets/_Sockets/HammerableNail.cs
+++ Assets/_Sockets/HammerableNail.cs
@@ -3,90 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 
-public class HammerableNail : MonoBehaviour
+public class HammerableNail : Socketable
 {
     private bool canBeNailed = true;
-    // we can use the onAttachedToHand/onDetachedFromHand
-    //   events on an interactable to trigger socket attach
-    //   and release methods.
-    private Interactable _interactable;
-    // a reference to the objects rigidbody will allow us to
-    //   disable gravity so that the object hovers in the socket.
-    private Rigidbody _rigidbody;
-    // a socket is visible when an object is inside of its
-    //   collision boundary.
-    private NailSocket _visibleSocket;
-
     private NailSocket _lastSocket;
-
-    // these flags are useful for managing the state of a
-    //   socketable object.
-    private bool _inSocketZone;
-    public bool _attachedToSocket;
-
     public bool _fromNailgun;
-
-    void Awake()
-    {
-        // get handle for steamvr interactable script
-        _interactable = GetComponent<Interactable>();
-
-        // register socket functions with interactable events
-        _interactable.onAttachedToHand += DetachFromSocket;
-        _interactable.onDetachedFromHand += AttachToSocket;
-
-        // get handle for attached rigidbody to disable
-        //   gravity when needed
-        _rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        // if attached to socket, disable gravity to
-        //   'hover' object and keep at socket position
-        if (_visibleSocket != null)
-        {
-            _lastSocket = _visibleSocket;
-        }
-        if (_attachedToSocket)
-        {
-            if (_rigidbody != null)
-            {
-                _rigidbody.useGravity = false;
-            }
-            // just use the socket position
-            if (_lastSocket != null)
-            {
-                transform.position = _lastSocket.transform.position;
-                transform.rotation = _lastSocket.transform.rotation;
-            }
-        }
-
-    }
-
-    private void AttachToSocket(Hand hand)
-    {
-        // if inside socket zone while being let go from hand, attach to socket.
-        if (_inSocketZone && !_visibleSocket.HoldingSocketable)
-        {
-            _attachedToSocket = true;
-            _visibleSocket.HoldingSocketable = true;
-            Destroy(gameObject.GetComponent<Throwable>());
-            Destroy(gameObject.GetComponent<Interactable>());
-        }
-    }
-
-    private void DetachFromSocket(Hand hand)
-    {
-        // if attached to socket while being grabbed by hand, release from socket.
-        if (_attachedToSocket)
-        {
-            _attachedToSocket = false;
-            _visibleSocket.HoldingSocketable = false;
-            _rigidbody.useGravity = true;
-        }
-
-    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -144,39 +65,39 @@ public class HammerableNail : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        // don't run unless colliding with a socket.
-        if (other.gameObject.name == "NailSocket" || other.gameObject.name.Contains("NailSocket ("))
-        {
-            // Remove any velocity
-            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    // don't run unless colliding with a socket.
+    //    if (other.gameObject.name == "NailSocket" || other.gameObject.name.Contains("NailSocket ("))
+    //    {
+    //        // Remove any velocity
+    //        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    //        gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
 
-            // set other values
-            canBeNailed = true;
-            _inSocketZone = true;
-            _visibleSocket = other.GetComponent<NailSocket>();
-            _lastSocket = _visibleSocket;
-            if (_fromNailgun)
-            {
-                _attachedToSocket = true;
-                _visibleSocket.HoldingSocketable = true;
-                TryConnect();
-            }
-        }
-    }
+    //        // set other values
+    //        canBeNailed = true;
+    //        _inSocketZone = true;
+    //        _visibleSocket = other.GetComponent<NailSocket>();
+    //        _lastSocket = _visibleSocket;
+    //        if (_fromNailgun)
+    //        {
+    //            _attachedToSocket = true;
+    //            _visibleSocket.HoldingSocketable = true;
+    //            TryConnect();
+    //        }
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider other)
-    {
-        // don't run unless colliding with a socket.
-        if (other.gameObject.name == "NailSocket" || other.gameObject.name.Contains("NailSocket ("))
-        {
-            canBeNailed = false;
-            _inSocketZone = false;
-            _visibleSocket = null;
-        }
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    // don't run unless colliding with a socket.
+    //    if (other.gameObject.name == "NailSocket" || other.gameObject.name.Contains("NailSocket ("))
+    //    {
+    //        canBeNailed = false;
+    //        _inSocketZone = false;
+    //        _visibleSocket = null;
+    //    }
+    //}
 
     private void TryConnect()
     {
@@ -191,7 +112,7 @@ public class HammerableNail : MonoBehaviour
             Destroy(GetComponent<CapsuleCollider>());
             // Reparent this to the board, then destroy the socket
             gameObject.transform.SetParent(hit.gameObject.transform.parent);
-            Destroy(_visibleSocket.gameObject);
+            //Destroy(_visibleSocket.gameObject);
             Destroy(GetComponent<Rigidbody>());
             // Fix the position and make the visible socket null
             transform.position = newpos.position;
